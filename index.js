1e27648f36fcf3f247c5cb5561c01165b8f62db8
ibrahimmdef <ibrahimmusethis@gmail.com>
2025-10-25 13:48:08 +0400

    weather-app design v1

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import axios from "axios";
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(port, () => {
+  console.log(true);
+});
